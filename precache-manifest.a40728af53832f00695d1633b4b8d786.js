4b0a98cf088df63d70265f082ab12b474eb20a3c
Ramon de Paula Marques <ramonpm@gmail.com>
2019-09-08 20:49:31 -0300

    Updates

---
+++ precache-manifest.a40728af53832f00695d1633b4b8d786.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "335cf8431ff874a81986fb843928ff11",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dd9fc116b34a24c6594f",
+    "url": "/static/css/main.aa614c5d.chunk.css"
+  },
+  {
+    "revision": "3660b8ce798c6b337889",
+    "url": "/static/js/2.d6261458.chunk.js"
+  },
+  {
+    "revision": "dd9fc116b34a24c6594f",
+    "url": "/static/js/main.44422a79.chunk.js"
+  },
+  {
+    "revision": "91f592772f010ea7ba0f",
+    "url": "/static/js/runtime~main.fc145892.js"
+  }
+]);
