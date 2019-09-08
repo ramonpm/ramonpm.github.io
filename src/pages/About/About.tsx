56d6d996da1a6ae2ee5edcc7902dc62136b1f1b3
Ramon de Paula Marques <ramonpm@gmail.com>
2019-09-08 20:36:04 -0300

    to_react

---
+++ src/pages/About/About.tsx
@@ -0,0 +1,69 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div className="container mt-5">
+      <h1>Things I’m good at</h1>
+      <h2>1. Assisting people</h2>
+      <p>
+        Whenever someone comes to me requesting help it’s like turning some switch on in my mind that fuels my motivation to solve whatever they need.
+      </p>
+
+      <h2>2. Learning</h2>
+      <p>
+        Learning new things was always an easy thing for me.
+      </p>
+
+      <h2>3. Discussing</h2>
+      <p>
+        People around me usually have big moments discussing things with me because they know good conclusions will come out of this.
+      </p>
+
+      <h2>4. Investigating</h2>
+      <p>
+        Life plays good mysteries on us and I like to solve them.
+      </p>
+
+      <h2>5. Creating and enhancing strategies</h2>
+      <p>
+        Competitive games were always my thing.
+        Football back on school days, my colleagues use to say I was the best. Card games, video games, and any E-sports games I’ve played I was a challenging adversary.
+      </p>
+
+      <h2>Collaboration</h2>
+      <p>
+        I believe a single person can achieve good results but I know for sure that a team can achieve extraordinary results. Whenever I see I can contribute with anything to help a team member succeed I’ll do it.
+      </p>
+
+      <h2>7. Commitment</h2>
+      <p>
+        I can’t even count how many times I’ve done things that ordinary people would never do. When I engage in doing something I’ll go hard on it…
+        Waking up at 4:30 AM to take my girlfriend (wife now) at her place so we could go run by the lake together.
+        Waking up at 3:00 AM to play an event in an online game.
+        Waking up at 5:00 AM to attend a meeting at Work when I was dismissed of it, so I could just skip it without a problem
+      </p>
+
+      <h2>8. Programming</h2>
+      <p>
+        It’s a natural thing to me, create scripts, think about how data comes in and how it should get out, don’t give up on failures, be able to change the mindset when I can’t move, I know all of this is hard to a lot of people but easy to me.
+      </p>
+
+      <h2>9. Finance</h2>
+      <p>
+        I’ve always been financially well. Since I was a Kid I remember I could raise money to whatever I wanted, and I never got it free from anyone. I had to work for it. Wash the dishes, wash my father’s car then buying a box of gums and selling them to colleagues at school by a higher price is an example of how I used to do in order to raise money.
+        Then later when I started the real work, to have a job, I knew I had to save a good part of my salary in order to make more money later, make investments, and buy expensive stuff like my own house. Nowadays, I own shares of several companies in the Stock Market, have government bonds, and started to talk with successful Angel investors to learn from them on how to become one.
+      </p>
+
+      <h2>10. Music</h2>
+      <p>
+        A passion I have, I like any kind of music, that sometimes makes people annoyed because usually they expect someone to have a real preference and proudly say something like "I like Rock N' Roll" or "I like classical Music".
+        
+        I, Ramon, like music, even if it's just someone playing with a car horn, if it has rythm and a melody I'll like it.
+
+        Also it worth to mention that I've studied music too, I play Guitar and Piano, and I Sing as well.
+      </p>
+    </div>
+  );
+}
+
+export default About;
